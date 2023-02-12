8a85707c71a10f376ed7bd021f064faf84da5654
kongnyc <kongnyc>
2023-02-12 13:57:43 -0500

    fetch data from API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import SearchBar from './components/SearchBar/SearchBar';
 import Users from './components/Users/Users';
 import './App.css';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
+
+const API_URL = 'https://users-app-backend.onrender.com/users';
 
 function App() {
   // TODO: Fetch data here
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      //Fetch data here from API
+      const response = await axios.get(API_URL);
+      // debugger;
+      setUsers(response.data.data);
+  }
+    fetchData();
+  },[])
 
   return (
     <div className="App">
-      <h1>Our Users</h1>
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.country}</p>
+            <p>{user.company}</p>
+          </div>
+        ))}
+      </div>
+
       <SearchBar />
       <Users />
     </div>
